f4032e08a9b6cbc2c407fa35e19d28770a5fa2e4
stephen <stepheng323@gmail.com>
2019-06-02 11:18:44 +0000

    refactor(es6): use  es6 syntax

--- api/v1/app.js
+++ api/v1/app.js
@@ -1,7 +1,6 @@
-/* eslint-disable no-unused-vars */
-const express = require('express');
+import express from 'express';
 
-const router = require('./routes/signup');
+import router from './routes/signup';
 
 const app = express();
 app.use(router);
@@ -10,4 +9,4 @@ const port = process.env.PORT || 3000;
 // eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Server running on port ${port}...`));
 
-module.exports = app;
+export default app;
