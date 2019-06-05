d74076c5f229b3dbddb87d2ff92c2f1af3437aa1
stephen <stepheng323@gmail.com>
2019-06-05 14:14:05 +0000

    chore(refactor):refactor signup controller

--- controller/signup.js
+++ controller/signup.js
@@ -1,53 +1,77 @@
 /* eslint-disable class-methods-use-this */
 import Joi from 'joi';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import users from '../db/users';
 
-const schema = {
-  id: Joi.number(),
-  first_name: Joi.string().required(),
-  last_name: Joi.string().required(),
-  email: Joi.string().required(),
-  password: Joi.string().required(),
-  address: Joi.string().required(),
-  is_admin: Joi.boolean().required(),
-};
-
-class Controller {
-  postUser(req, res) {
-    const result = Joi.validate(req.body, schema);
+class Signup {
+  createUser(req, res) {
+    const userSchema = {
+      id: Joi.number(),
+      first_name: Joi.string().required(),
+      last_name: Joi.string().required(),
+      email: Joi.string().required(),
+      password: Joi.string().required(),
+      address: Joi.string().required(),
+      is_admin: Joi.boolean().required(),
+    };
+    const result = Joi.validate(req.body, userSchema);
 
     if (result.error) {
-      res.status(400).send({
+      res.status(400).json({
         status: 400,
         error: result.error.details[0].message,
       });
       return;
     }
+    const found = users.find(e => e.email === req.body.email);
 
-    const user = {
-      id: users.length + 1,
-      first_name: req.body.first_name,
-      last_name: req.body.last_name,
-      email: req.body.email,
-      password: req.body.password,
-      address: req.body.address,
-      is_admin: req.body.is_admin,
-    };
+    if (found) {
+      res.status(409).json({
+        status: 409,
+        message: 'email has been used ',
+      });
+      return;
+    }
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+      if (err) {
+        return res.status(500).json({
+          error: err,
+        });
+      }
 
-    users.push(user);
+      const user = {
+        id: users.length + 1,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        password: hash,
+        address: req.body.address,
+        is_admin: req.body.is_admin,
+      };
 
-    res.status(200).send({
-      status: 200,
-      data: {
-        token: '45erkjherht45495783',
-        id: user.id,
-        first_name: user.first_name,
-        last_name: user.last_name,
-        email: user.email,
-      },
+      users.push(user);
+      const token = jwt.sign(
+        {
+          email: user.email,
+          id: user.id,
+        },
+        'thefellowship',
+        { expiresIn: '1hr' },
+      );
+      return res.status(201).json({
+        status: 201,
+        data: {
+          token,
+          id: user.id,
+          first_name: user.first_name,
+          last_name: user.last_name,
+          email: user.email,
+        },
+      });
     });
   }
 }
 
-const controller = new Controller();
-export default controller;
+const signup = new Signup();
+export default signup;
