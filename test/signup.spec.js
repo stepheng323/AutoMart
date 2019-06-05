e6141f033f9cb21883f794708ad591589bc27335
stephen <stepheng323@gmail.com>
2019-06-05 13:47:49 +0000

    test(separate concern):separate tests

--- test/signup.spec.js
+++ test/signup.spec.js
@@ -7,21 +7,20 @@ import app from '../app';
 chai.use(chaihttp);
 
 describe('Auth', () => {
-  const mock = {
-    email: 'stepheng323@gmail.com',
-    first_name: 'abioduon',
-    last_name: 'oyebanji',
-    password: 'olaTUNDE',
-    address: '13',
-    is_admin: 'false',
-  };
   it('signup should create a user', () => {
     chai
       .request(app)
       .post('/api/v1/auth/signup')
-      .send(mock)
+      .send({
+        email: 'steiipheng323@gmail.com',
+        first_name: 'abioduon',
+        last_name: 'oyebanji',
+        password: 'olaTUNDE',
+        address: '13',
+        is_admin: 'false',
+      })
       .end((err, res) => {
-        expect(res.body).to.have.status(200);
+        expect(res.body).to.have.status(201);
         expect(res.body).to.be.a('object');
         expect(res.body.data).to.have.a.property('id');
         expect(res.body.data)
