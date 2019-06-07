04297d15127d95e54a43887304387758fdf51299
stephen <stepheng323@gmail.com>
2019-06-07 23:00:47 +0000

    ft(route):add a post car route

--- routes/route.js
+++ routes/route.js
@@ -2,13 +2,15 @@ import express from 'express';
 
 import signin from '../controller/signin';
 import signup from '../controller/signup';
+import carscreate from '../controller/createCar';
+import checkAuth from '../middleware/checkAuth';
 
 const router = express.Router();
 router.use(express.json());
 router.use(express.urlencoded({ extended: false }));
 
-
 router.post('/api/v1/auth/signup', signup.createUser);
 router.post('/api/v1/auth/signin', signin.signIn);
+router.post('/api/v1/car', checkAuth, carscreate.createCar);
 
 export default router;
