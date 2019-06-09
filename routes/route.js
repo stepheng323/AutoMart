932753f0878810b9bf77440b9d42e7c9609d5a86
stephen <stepheng323@gmail.com>
2019-06-09 09:27:56 +0000

    feature(route):add an order route

--- routes/route.js
+++ routes/route.js
@@ -3,6 +3,7 @@ import express from 'express';
 import signin from '../controller/signin';
 import signup from '../controller/signup';
 import carscreate from '../controller/createCar';
+import orders from '../controller/postOrder';
 import checkAuth from '../middleware/checkAuth';
 
 const router = express.Router();
@@ -12,5 +13,6 @@ router.use(express.urlencoded({ extended: false }));
 router.post('/api/v1/auth/signup', signup.createUser);
 router.post('/api/v1/auth/signin', signin.signIn);
 router.post('/api/v1/car', checkAuth, carscreate.createCar);
+router.post('/api/v1/order', checkAuth, orders.createOrder);
 
 export default router;
