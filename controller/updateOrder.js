66e4590e5d38322a7016a5b23d84ce6d00d89ae8
stephen <stepheng323@gmail.com>
2019-06-09 15:55:46 +0000

    feature(controller):add update order endpoint

---
+++ controller/updateOrder.js
@@ -0,0 +1,49 @@
+import Joi from 'joi';
+import { orders } from '../db/orders';
+
+class UpdateOrders {
+  // eslint-disable-next-line class-methods-use-this
+  updateOrders(req, res) {
+    const order = orders.find(o => o.id === parseInt(req.params.id, 10));
+    if (!order) {
+      res.status(404).json({
+        status: 404,
+        error: 'order not found',
+      });
+      return;
+    }
+
+    const schema = {
+      new_price_offered: Joi.number().required(),
+    };
+    const result = Joi.validate(req.body, schema);
+
+    if (result.error) {
+      res.status(400).json({
+        status: 400,
+        error: result.error.details[0].message,
+      });
+      return;
+    }
+    if (order.status === 'pending') {
+      order.new_price_offered = req.body.new_price_offered;
+      res.status(200).json({
+        status: 200,
+        data: {
+          id: order.id,
+          car_id: order.car_id,
+          status: order.status,
+          old_price_offered: order.amount,
+          new_price_offered: order.new_price_offered,
+        },
+      });
+    } else {
+      res.status(333).json({
+        status: 333,
+        error: 'Order can only be updated when your status is pending',
+      });
+    }
+  }
+}
+const ordersUpdate = new UpdateOrders();
+export default ordersUpdate;
