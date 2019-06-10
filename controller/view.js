51d6e38cca1df12f74ecf5a71608c5b229cf7fee
stephen <stepheng323@gmail.com>
2019-06-10 07:52:07 +0000

    feature(controller):add controller to  view unsold cars

--- controller/view.js
+++ controller/view.js
@@ -18,6 +18,24 @@ class View {
       },
     });
   }
+
+  // eslint-disable-next-line class-methods-use-this
+  unsold(req, res) {
+    const available = cars.filter(a => a.status === req.query.status);
+    if (!available) {
+      res.status(404).json({
+        status: 404,
+        error: 'no cars found',
+      });
+      return;
+    }
+    res.status(200).json({
+      status: 200,
+      data: {
+        available,
+      },
+    });
+  }
 }
 const view = new View();
 export default view;
