0dc76681368b442eade058dcb1989049e964b153
stephen <stepheng323@gmail.com>
2019-06-17 12:22:05 +0000

    refactor(import):replace db with model in import

---
+++ model/cars.js
@@ -0,0 +1,30 @@
+import joi from 'joi';
+
+const carSchema = {
+  id: joi.number(),
+  owner: joi.number(),
+  created_on: joi.date(),
+  state: joi.string().required(),
+  status: joi.string().required(),
+  price: joi.number().required(),
+  manufacturer: joi.string().required(),
+  model: joi.string(),
+  body_type: joi.string(),
+  product_image: joi.string(),
+};
+
+const cars = [
+  {
+    id: 1,
+    owner: 1,
+    created_on: new Date(),
+    state: 'used',
+    status: 'available',
+    price: 50000,
+    manufacturer: 'toyota',
+    model: 'camry',
+    body_type: 'car',
+  },
+];
+
+export { cars, carSchema };
