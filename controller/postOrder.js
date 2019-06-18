4516c6e36478d378dd761e531d678f27a6c7413f
stephen <stepheng323@gmail.com>
2019-06-18 07:09:26 +0000

    feature(controller):add purchase order with database to persist data

--- controller/postOrder.js
+++ controller/postOrder.js
@@ -1,6 +1,9 @@
 import Joi from 'joi';
-import { orders, orderSchema } from '../model/orders';
-import { cars } from '../model/cars';
+import dotenv from 'dotenv';
+import pg from 'pg';
+import { orderSchema } from '../model/orders';
+
+dotenv.config();
 
 class CreateOrders {
   // eslint-disable-next-line class-methods-use-this
@@ -15,34 +18,75 @@ class CreateOrders {
       return;
     }
 
-    const decoded = req.userData;
-
-    const order = {
-      id: orders.length + 1,
-      buyer: decoded.id,
-      car_id: req.body.car_id,
-      amount: req.body.amount,
-      status: 'pending',
+    const config = {
+      user: 'abiodun',
+      database: process.env.DATABASE,
+      password: process.env.PASSWORD,
+      port: process.env.DB_PORT,
+      max: 10,
+      idleTimeoutMillis: 30000,
     };
-    orders.push(order);
-    const found = cars.find(c => c.id === order.car_id);
-    if (!found) {
-      res.status(404).json({
-        status: 404,
-        error: 'car not found',
-      });
-    }
 
-    res.status(201).json({
-      status: 201,
-      data: {
-        id: order.id,
-        car_id: order.car_id,
-        created_on: found.created_on,
-        status: order.status,
-        price: found.price,
-        price_offerd: order.amount,
-      },
+    const pool = new pg.Pool(config);
+    pool.on('connect', () => {
+      // eslint-disable-next-line no-console
+      console.log('connected to the database');
+    });
+
+    pool.connect((err, client, done) => {
+      if (err) {
+        // eslint-disable-next-line no-console
+        console.log('unable to connect to pool');
+        return;
+      }
+      const decoded = req.userData;
+      const order = {
+        buyer: decoded.id,
+        car_id: req.body.car_id,
+        amount: req.body.amount,
+        status: 'pending',
+      };
+
+      const query = 'INSERT INTO orders(buyer, car_id, amount, status) VALUES ($1, $2, $3, $4) RETURNING *';
+      const value = [order.buyer, order.car_id, order.amount, order.status];
+
+      client.query(query, value, (queryError, queryResult) => {
+        done();
+        if (queryError) {
+          res.status(400).json({
+            status: 500,
+            errorr: 'server error',
+          });
+          return;
+        }
+
+        const dbResult = queryResult.rows[0];
+        const query2 = 'SELECT * FROM cars WHERE id = $1';
+        const id = [dbResult.car_id];
+
+        client.query(query2, id, (queryError2, queryResult2) => {
+          done();
+          if (queryError2) {
+            res.status(400).json({
+              status: 500,
+              error: 'server error',
+            });
+            return;
+          }
+          const dbResult2 = queryResult2.rows[0];
+          res.status(201).json({
+            status: 201,
+            data: {
+              id: dbResult.id,
+              car_id: dbResult.car_id,
+              created_on: dbResult2.created_on,
+              status: dbResult.status,
+              price: dbResult2.price,
+              price_offerd: dbResult.amount,
+            },
+          });
+        });
+      });
     });
   }
 }
