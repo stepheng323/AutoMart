2043373c004f7a70f049679a8727febd93f3d163
stephen <stepheng323@gmail.com>
2019-06-20 06:12:17 +0000

    feature(controller):add feature to view a specific car from database

--- controller/view.js
+++ controller/view.js
@@ -1,4 +1,6 @@
 import pg from 'pg';
+import users from '../model/users';
+import cars from '../model/cars';
 
 class View {
   // eslint-disable-next-line class-methods-use-this
@@ -55,6 +57,112 @@ class View {
         next();
       });
     });
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  soldOrAvailable(req, res, next) {
+    const decoded = req.userData;
+    const user = users.find(u => u.id === decoded.id);
+    if (!user) {
+      res.status(403).json({
+        status: 403,
+        error: 'you must be logged in',
+      });
+      return;
+    }
+
+    if (user.is_admin) {
+      const availableOrSold = cars.filter(a => a.status === 'available' || 'sold');
+      if (!availableOrSold) {
+        res.status(404).json({
+          status: 404,
+          error: 'no cars found',
+        });
+        return;
+      }
+      res.status(200).json({
+        status: 200,
+        data: {
+          availableOrSold,
+        },
+      });
+    } else {
+      next();
+    }
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  unsold(req, res, next) {
+    if (req.query.min_price && req.query.max_price) {
+      next();
+      return;
+    }
+
+    const available = cars.filter(a => a.status === req.query.status);
+    if (!available) {
+      res.status(404).json({
+        status: 404,
+        error: 'no cars found',
+      });
+      return;
+    }
+    res.status(200).json({
+      status: 200,
+      data: {
+        available,
+      },
+    });
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  priceRange(req, res) {
+    const available = cars.filter(a => a.status === req.query.status);
+    const filtered = available.filter(
+      f => f.price <= req.query.max_price && f.price >= req.query.min_price,
+    );
+    if (!filtered) {
+      res.status(404).json({
+        status: 404,
+        error: 'no cars found',
+      });
+      return;
+    }
+    res.status(200).json({
+      status: 200,
+      data: {
+        filtered,
+      },
+    });
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  deleteCar(req, res) {
+    const decoded = req.userData;
+    const user = users.find(u => u.id === decoded.id);
+
+    if (user.is_admin) {
+      const car = cars.find(c => c.id === parseInt(req.params.car_id, 10));
+      if (!car) {
+        res.status(404).json({
+          status: 404,
+          error: 'car not found',
+        });
+        return;
+      }
+
+      const index = cars.indexOf(car);
+      cars.splice(index, 1);
+
+      res.status(200).json({
+        status: 200,
+        data: 'Car Ad succefully deleted',
+      });
+    } else {
+      res.status(403).json({
+        status: 403,
+        error: 'Only admin can delete a car',
+      });
+    }
   }
 }
 const view = new View();
