aa8cac86638dda0d4bec00a57154057c4faa4bd6
stephen <stepheng323@gmail.com>
2019-06-21 12:10:57 +0000

    feature(controller):add seller can update their car price (database)

--- controller/updatePrice.js
+++ controller/updatePrice.js
@@ -1,29 +1,11 @@
 import joi from 'joi';
+import pg from 'pg';
 import { cars } from '../model/cars';
 import users from '../model/users';
 
 class UpdatePrice {
   // eslint-disable-next-line class-methods-use-this
   priceUpdate(req, res) {
-    const car = cars.find(c => c.id === parseInt(req.params.id, 10));
-
-    if (!car) {
-      res.status(404).json({
-        status: 404,
-        error: 'car not found',
-      });
-      return;
-    }
-    // checking to see if the user is the car owner
-    const decoded = req.userData;
-
-    if (decoded.id !== car.owner) {
-      res.status(404).json({
-        status: 404,
-        error: 'you can only update cars you posted',
-      });
-      return;
-    }
     const schema = {
       price: joi.number().required(),
     };
@@ -36,27 +18,97 @@ class UpdatePrice {
       });
       return;
     }
-    if (car.status !== 'sold') {
-      car.price = req.body.price;
-      res.status(200).json({
-        status: 200,
-        data: {
-          id: car.id,
-          email: decoded.email,
-          created_on: car.created_on,
-          manufacturer: car.manufacturer,
-          model: car.model,
-          price: car.price,
-          state: car.state,
-          status: car.status,
-        },
+    // ready Database for query
+    const config = {
+      user: 'abiodun',
+      database: process.env.DATABASE,
+      password: process.env.PASSWORD,
+      port: process.env.DB_PORT,
+      max: 10,
+      idleTimeoutMillis: 30000,
+    };
+
+    const pool = new pg.Pool(config);
+
+    pool.on('connect', () => {
+      // eslint-disable-next-line no-console
+      console.log('connected to the Database');
+    });
+
+    pool.connect((err, client, done) => {
+      if (err) {
+        res.status(400).json({
+          status: 400,
+          error: 'could not connect to the pool',
+        });
+        return;
+      }
+      const query = 'SELECT * FROM cars WHERE id = $1';
+      const value = [req.params.id];
+
+      client.query(query, value, (queryError, results) => {
+        if (queryError) {
+          res.status(500).json({
+            status: 500,
+            error: `${queryError}`,
+          });
+          return;
+        }
+        const decoded = req.userData;
+        const car = results.rows[0];
+        if (!car) {
+          res.status(404).json({
+            status: 404,
+            error: 'car not found',
+          });
+          return;
+        }
+
+        // check to see if the user is the car owner
+        if (decoded.id !== car.owner) {
+          res.status(403).json({
+            status: 403,
+            error: 'you can only update cars you posted',
+          });
+          return;
+        }
+        if (car.status !== 'sold') {
+          const query2 = 'UPDATE cars SET price =$1 WHERE id = $2 RETURNING *';
+          const value2 = [req.body.price, req.params.id];
+
+          client.query(query2, value2, (queryError2, result2) => {
+            done();
+            if (queryError2) {
+              res.status(500).json({
+                status: 500,
+                error: `${queryError2}`,
+              });
+              return;
+            }
+            const car2 = result2.rows[0];
+
+            res.status(200).json({
+              status: 200,
+              data: {
+                id: car2.id,
+                email: decoded.email,
+                created_on: car2.created_on,
+                manufacturer: car2.manufacturer,
+                model: car2.model,
+                price: car2.price,
+                state: car2.state,
+                status: car2.status,
+              },
+            });
+          });
+        } else {
+          res.status(404).json({
+            status: 404,
+            error: 'car can only be updated when status is available',
+          });
+        }
       });
-    } else {
-      res.status(404).json({
-        status: 404,
-        error: 'car can only be updated when your status is available',
-      });
-    }
+    });
   }
 
   // eslint-disable-next-line class-methods-use-this
