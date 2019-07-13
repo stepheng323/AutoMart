905ad4ac6d9a9108bbdcec79c48da995e036cec8
stephen <stepheng323@gmail.com>
2019-07-13 02:30:01 +0000

    chore(travis):update travis database

--- src/config.js
+++ src/config.js
@@ -25,7 +25,7 @@ const travis = {
   user: process.env.USER,
   database: process.env.DATABASE,
   password: process.env.PASSWORD,
-  port: process.env.PORT,
+  port: process.env.DB_PORT,
   max: 20,
   idleTimeoutMillis: 30000,
 };
