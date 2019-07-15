006f1f38190000819aee4761fa3ae8488cb71bee
stephen <stepheng323@gmail.com>
2019-07-15 15:10:17 +0000

    test(signin):add valid user for test

--- test/auth.spec.js
+++ test/auth.spec.js
@@ -29,23 +29,23 @@ describe('Auth', () => {
         done();
       });
   });
-});
-it('sign a user in and return token', (done) => {
-  chai
-    .request(app)
-    .post('/api/v1/auth/signin')
-    .send({
-      email: 'temi@gmail.com',
-      password: 'biodun',
-    })
-    .end((err, res) => {
-      expect(res.body).to.have.status(200);
-      expect(res.body).to.be.a('object');
-      expect(res.body.data).to.have.a.property('token');
-      expect(res.body.data).to.have.a.property('id');
-      expect(res.body.data).to.have.a.property('first_name');
-      expect(res.body.data).to.have.a.property('last_name');
-      expect(res.body.data).to.have.a.property('email');
-      done();
-    });
+  it('sign a user in and return token', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/auth/signin')
+      .send({
+        email: 'temi@gmail.com',
+        password: 'biodun',
+      })
+      .end((err, res) => {
+        expect(res.body).to.have.status(200);
+        expect(res.body).to.be.a('object');
+        expect(res.body.data).to.have.a.property('token');
+        expect(res.body.data).to.have.a.property('id');
+        expect(res.body.data).to.have.a.property('first_name');
+        expect(res.body.data).to.have.a.property('last_name');
+        expect(res.body.data).to.have.a.property('email');
+        done();
+      });
+  });
 });
