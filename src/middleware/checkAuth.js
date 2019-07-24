3d91c5e64095aa39757b894481725a2aa1665e3a
stephen <stepheng323@gmail.com>
2019-07-24 05:18:14 +0000

    chore(token):detects token before assigining it

--- src/middleware/checkAuth.js
+++ src/middleware/checkAuth.js
@@ -1,13 +1,26 @@
 import jwt from 'jsonwebtoken';
 
 const auth = (req, res, next) => {
-  try {
+  if (req.headers.authorization) {
     const token = req.headers.authorization.split(' ')[1];
-    const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
-    req.userData = decoded;
-    next();
-  } catch (error) {
-    res.status(401).json({
+    if (!token) {
+      return res.status(401).json({
+        status: 401,
+        error: 'Access Denied',
+      });
+    }
+    try {
+      const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+      req.userData = decoded;
+      next();
+    } catch (error) {
+      res.status(400).json({
+        status: 400,
+        error: 'Invalid Token',
+      });
+    }
+  } else {
+    return res.status(401).json({
       status: 401,
       error: 'Access Denied',
     });
