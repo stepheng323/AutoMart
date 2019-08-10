5e8cd766e06a7065a033e99ddf3351674f7e5bdf
stephen <stepheng323@gmail.com>
2019-08-10 10:45:28 +0100

    refactor test

--- test/auth.spec.js
+++ test/auth.spec.js
@@ -6,29 +6,28 @@ import app from '../src/app';
 
 chai.use(chaihttp);
 describe('Auth', () => {
-  // it('signup should create a user', (done) => {
-  //   chai
-  //     .request(app)
-  //     .post('/api/v1/auth/signup')
-  //     .send({
-  //       email: 'henyhart582@swellview.com',
-  //       first_name: 'henry',
-  //       last_name: 'hart',
-  //       password: 'captainman',
-  //       address: '2345 avenue mancave swellview',
-  //     })
-  //     .end((err, res) => {
-  //       console.log(res);
-  //       expect(res.body).to.have.status(201);
-  //       expect(res.body).to.be.a('object');
-  //       expect(res.body.data).to.have.a.property('token');
-  //       expect(res.body.data).to.have.a.property('id');
-  //       expect(res.body.data).to.have.a.property('first_name');
-  //       expect(res.body.data).to.have.a.property('last_name');
-  //       expect(res.body.data).to.have.a.property('email');
-  //       done();
-  //     });
-  // });
+  it('signup should create a user', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'henyhart582@swellview.com',
+        first_name: 'henry',
+        last_name: 'hart',
+        password: 'captainman',
+        address: '2345 avenue mancave swellview',
+      })
+      .end((err, res) => {
+        expect(res.body).to.have.status(201);
+        expect(res.body).to.be.a('object');
+        expect(res.body.data).to.have.a.property('token');
+        expect(res.body.data).to.have.a.property('id');
+        expect(res.body.data).to.have.a.property('first_name');
+        expect(res.body.data).to.have.a.property('last_name');
+        expect(res.body.data).to.have.a.property('email');
+        done();
+      });
+  });
   it('sign a user in and return token', (done) => {
     chai
       .request(app)
